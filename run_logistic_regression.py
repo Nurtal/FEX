4e4786cd5ae4cdb621dbff4ee75c960a8519fa79
Nurtal <nathan.foulquier.pro@gmail.com>
2024-12-02 08:22:33 +0100

    update logreg

--- run_logistic_regression.py
+++ run_logistic_regression.py
@@ -90,10 +90,60 @@ def run(data_file:str) -> dict:
     return results
 
 
+def extract_features(data_file:str) -> dict:
+    """Run Logistic regression to extract important features from data_file.
+    model is run using cross validation, feature importance as computed as the mean of absolute value of
+    each contribution accross the cross validation. 
+    
+    Args:
+        - data_file (str) : path to the data file, must contains a 'LABEL' columns
+
+    Returns:
+        - (dict) : feature to absolute value of contribution
+        
+    """
+
+    # load data
+    df = pd.read_csv(data_file)
+    X = df.drop(columns=["LABEL"])
+    nb = 0
+    label_to_nb = {}
+    for label in set(df['LABEL']):
+        label_to_nb[label] = nb
+        nb+=1
+    y = df["LABEL"].map(label_to_nb)
+
+    # cross validation split
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+    coefficients_list = []
+
+    # train on each fold
+    for train_idx, test_idx in cv.split(X, y):
+        X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+        y_train, y_test = y[train_idx], y[test_idx]
+        logreg = LogisticRegression(solver='liblinear', random_state=42)
+        logreg.fit(X_train, y_train)
+    
+        # extract coeff
+        coefficients_list.append(abs(logreg.coef_))
+
+    # compute mean of coefs
+    mean_coefficients = np.mean(coefficients_list, axis=0)
+
+    # sort and save
+    feature_to_coef = {}
+    for feature_name, coef in zip(list(X.keys()), mean_coefficients[0]):
+        feature_to_coef[feature_name] = abs(coef)
+    feature_to_coef = dict(sorted(feature_to_coef.items(), key=lambda item: item[1], reverse=True))
+
+    # return selected features and their contribution
+    return feature_to_coef
 
 
 if __name__ == "__main__":
 
     # Load the data
     data_file = "data/toy_data.csv"
-    s = run(data_file)
+    # s = run(data_file)
+    m = extract_features(data_file)
+    print(m)
